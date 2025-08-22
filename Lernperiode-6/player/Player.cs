831dd9c8167d946cf480d2479a0fa7c3526946cc
2003-Anonymous <J.Sommerhalder.inf24@stud.bbbaden.ch>
2025-08-22 11:50:53 +0200

    Add files via upload

---
+++ Lernperiode-6/player/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lernperiode_6.player
+{
+    internal class Player
+    {
+        public int health;
+        public int damage;
+        public int attackspeed;
+
+    }
+}
